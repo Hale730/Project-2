c6c8180bcee172f9202d8b3c79099ee87502f891
alr5342 <165952443+alr5342@users.noreply.github.com>
2024-04-15 14:59:02 -0500

    Update Flight
    Made the servo turn in the right direction and improved the code, especially for the beginning of Icarus' "flight"

--- TheSun.ino
+++ TheSun.ino
@@ -2,28 +2,34 @@
 
 Servo myservo; // servo variable
 const int buttonPin = 9; // copper wire is Pin 9
-const int lightSensor = A0;
+const int lightSensor = A2;
 int sensorVal; //photoresistor
-int buttonVal;
+int buttonVal = 0;
+int lastButtonVal = 0;
 
 void setup() {
   myservo.attach(2); // servo is Pin 2
   pinMode(buttonPin, INPUT); 
   Serial.begin(9600);
+  myservo.write(180);
 }
 
 void loop() {
   buttonVal = digitalRead(buttonPin);
   sensorVal = analogRead(lightSensor);
 
-  if (buttonVal == LOW) {
-    myservo.write(0); // nothing happens
-  }
-  else {
-    myservo.write (90); // servo turns/Icarus flies
+  if (buttonVal != lastButtonVal) {
+    // if the state has changed, increment the counter
+    if (buttonVal == LOW) {
+      myservo.write(180); // nothing happens
+    } else {
+      myservo.write (90); // servo turns/Icarus flies
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
   }
 
-  if (sensorVal >= 650); {
-    myservo.write (180);
-  }
+  // if (sensorVal >= 650); {
+  //    myservo.write (180);
+  // }
 }
